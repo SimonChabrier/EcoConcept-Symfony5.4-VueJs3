79e94ef716f404bf6eb6f1ba66373bed9f26cca6
Simon Chabrier <simonchabrier@gmail.com>
2023-01-09 18:30:57 +0100

    pagination v1

--- assets/views/ApiTpl.vue
+++ assets/views/ApiTpl.vue
@@ -6,7 +6,7 @@
             <span class="appSpan fill">Fetch data from Json Placeholder</span>
         </section>
 
-        <div v-for="post in posts" :key="post.id">
+        <div v-for="post in pagePosts" :key="post.id">
 
             <section class="section appLight flexRow">
                 <h3>{{ post.title }}</h3>
@@ -21,6 +21,11 @@
                 </ButtonComponent>
             </section>
 
+        </div>
+
+        <div class="pagination">
+            <ButtonComponent size="xs" @click="nextPage">Prev</ButtonComponent>
+            <ButtonComponent size="xs" @click="previousPage">Next</ButtonComponent>
         </div>
 
     </div>
@@ -45,6 +50,17 @@ export default {
         ButtonComponent,
     },
 
+    data () {
+      return {
+        //allPosts: [],
+        pages: [],
+        pagePosts: [],
+        totalPosts: this.posts.length,
+        currentPage: 0,
+        itemsPerPage: 3,
+      }
+    },
+
     setup() {
         // j'apelle le store avec son nom usePostStore
         const postStore = usePostStore()
@@ -55,6 +71,60 @@ export default {
         // je retourne les posts pour boucler dessus
         return { posts };
     },
+
+    methods: {
+
+        //action de pagination
+        nextPage() {
+            this.currentPage++;
+            this.appendPostsToPage();
+        },
+        previousPage() {
+            this.currentPage--;
+            this.appendPostsToPage();
+        },
+        // calculer le nombre de pages et crée un tableau avec des index de 0 à n
+        setPages() {
+            let index = 0;
+            let numberOfPages = Math.ceil(this.totalPosts / this.itemsPerPage);
+            for (index; index < numberOfPages; index++) {
+                this.pages.push(index); 
+            }
+        },
+        // ajouter les recettes à la page courante en fonction du nombre de recettes par page : itemsPerPage
+        // et de la valeur de la page courante : currentPage
+        appendPostsToPage() {
+            let startIndex = this.currentPage * this.itemsPerPage;
+            let endIndex = startIndex + this.itemsPerPage;
+            this.pagePosts = this.posts.slice(startIndex, endIndex);
+            
+            // afficher masquer les boutons suivant et précédent si on est au début ou à la fin
+            // this.currentPage === 0 ? this.hidePreviousButton = true : this.hidePreviousButton = false;
+            // endIndex >= this.totalPosts ? this.hideNextButton = true : this.hideNextButton = false; 
+        }
+
+    },
+
+    mounted() {
+        
+
+        // si j'ai des posts 
+        if (this.posts.length > 0 ) {
+            this.totalPosts = this.posts.length;
+            this.setPages();
+            this.appendPostsToPage();
+        } else {
+            // sinon je les récupère
+            // this.fetchPosts()
+        }
+
+        console.log(this.pagePosts)
+        console.log(this.posts)
+    }
+
+
+
+
 }
 </script>
 
